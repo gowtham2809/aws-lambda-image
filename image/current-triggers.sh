ab5ec11fc97d09cc5d6bd20d5f8459c958444699
Gowtham Raj <gowtham@tartlabs.com>
2020-07-16 21:25:59 +0530

    added pulsedin prod support

--- image/current-triggers.sh
+++ image/current-triggers.sh
@@ -1,4 +1,4 @@
-npm run add-s3-handler --s3_bucket="pulsedin-dev-storage" --s3_prefix="storage/" --s3_suffix=".jpg"
-npm run add-s3-handler --s3_bucket="pulsedin-dev-storage" --s3_prefix="storage/" --s3_suffix=".jpeg"
-npm run add-s3-handler --s3_bucket="pulsedin-dev-storage" --s3_prefix="storage/" --s3_suffix=".png"
-npm run add-s3-handler --s3_bucket="pulsedin-dev-storage" --s3_prefix="storage/" --s3_suffix=".gif"
+npm run add-s3-handler --s3_bucket="pulsedin-prod-storage" --s3_prefix="storage/" --s3_suffix=".jpg"
+npm run add-s3-handler --s3_bucket="pulsedin-prod-storage" --s3_prefix="storage/" --s3_suffix=".jpeg"
+npm run add-s3-handler --s3_bucket="pulsedin-prod-storage" --s3_prefix="storage/" --s3_suffix=".png"
+npm run add-s3-handler --s3_bucket="pulsedin-prod-storage" --s3_prefix="storage/" --s3_suffix=".gif"
