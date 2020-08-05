c9700d63c75f60a76563a6337c9e82673526f187
Gowtham Raj <gowtham@tartlabs.com>
2020-08-05 10:50:19 +0530

    fixed bucket name

--- image/current-triggers.sh
+++ image/current-triggers.sh
@@ -1,4 +1,4 @@
-npm run add-s3-handler --s3_bucket="onco-power-app-dev" --s3_prefix="storage/" --s3_suffix=".jpg"
-npm run add-s3-handler --s3_bucket="onco-power-app-dev" --s3_prefix="storage/" --s3_suffix=".jpeg"
-npm run add-s3-handler --s3_bucket="onco-power-app-dev" --s3_prefix="storage/" --s3_suffix=".png"
-npm run add-s3-handler --s3_bucket="onco-power-app-dev" --s3_prefix="storage/" --s3_suffix=".gif"
+npm run add-s3-handler --s3_bucket="onco-power-storage-dev" --s3_prefix="storage/" --s3_suffix=".jpg"
+npm run add-s3-handler --s3_bucket="onco-power-storage-dev" --s3_prefix="storage/" --s3_suffix=".jpeg"
+npm run add-s3-handler --s3_bucket="onco-power-storage-dev" --s3_prefix="storage/" --s3_suffix=".png"
+npm run add-s3-handler --s3_bucket="onco-power-storage-dev" --s3_prefix="storage/" --s3_suffix=".gif"
